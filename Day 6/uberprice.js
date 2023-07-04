0a8ee270c48f35f957091aed9c7e2698b6992588
Gladson Jerome <117337778+Gladson001@users.noreply.github.com>
2023-07-04 10:02:18 +0530

    js file uploaded

---
+++ Day 6/uberprice.js
@@ -0,0 +1,40 @@
+class UberPrice {
+    constructor(distance, cartype) {
+        this.distance = distance;
+        this.cartype = cartype;
+    }
+
+    calculateprice() {
+        let basicfare, costperkm, totalprice;
+
+        switch (this.cartype) {
+
+            case "ubermicro":
+                basicfare = 10;
+                costperkm = 5;
+                break;
+
+            case "ubermini":
+                basicfare = 8;
+                costperkm = 4;
+                break;
+
+            case "ubersedan":
+                basicfare = 20;
+                costperkm = 10;
+                break;
+
+            default:
+                throw new Error("Invalid car type.");
+
+        }
+
+        totalprice = basicfare + costperkm * this.distance;
+        return totalprice;
+    }
+
+}
+
+const fare = new UberPrice(10, "ubermicro");
+const price = fare.calculateprice();
+console.log(price);  
